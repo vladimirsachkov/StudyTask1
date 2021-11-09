dd0b83d27ce44f2a68fc3f316a0d6632ce9194be
chubukov <chubukov@cbtex.ru>
2021-11-09 06:04:10 +0300

    Initial commit

---
+++ src/ru/nsi/Main.java
@@ -0,0 +1,37 @@
+package ru.nsi;
+
+public class Main {
+
+    private static int[] generateSequence(int begin, int end, int count) {
+        int[] sequence = new int[count];
+        //Написать код наполняющий последовательность...
+
+        return sequence;
+    }
+
+    private static int getMinValue(int[] sequence) {
+        int minValue = 0;
+
+        return minValue;
+    }
+
+    private static int getMaxValue(int[] sequence) {
+        int maxValue = 0;
+
+        return maxValue;
+    }
+
+    private static int getAvgValue(int[] sequence) {
+        int avgValue = 0;
+
+        return avgValue;
+    }
+
+    public static void main(String[] args) {
+        //1. Считать аргументы командной строки
+
+        //2. Сгенерировать и вывести последовательность в одну строчку, разделяя элементы пробелом
+
+        //3. Вывести в одну строчку минимальное, максимальное и средние значения последовательности
+    }
+}
